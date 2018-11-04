ca3a0a5786ac291d9329fa8ffd29e8751e1aeb7d
Ben Meier <benmeier@fastmail.com>
2018-11-04 19:30:17 +0000

    Added first implementation of PDF build in docker

--- go.mod
+++ go.mod
@@ -1,12 +1,17 @@
 module github.com/AstromechZA/md-slides
 
 require (
+	github.com/alecthomas/assert v0.0.0-20170929043011-405dbfeb8e38 // indirect
 	github.com/alecthomas/chroma v0.3.0
+	github.com/alecthomas/colour v0.0.0-20160524082231-60882d9e2721 // indirect
+	github.com/alecthomas/repr v0.0.0-20181024024818-d37bc2a10ba1 // indirect
 	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
 	github.com/dlclark/regexp2 v1.1.6 // indirect
-	github.com/gorilla/context v0.0.0-20160226214623-1ea25387ff6f // indirect
-	github.com/gorilla/mux v1.6.1
+	github.com/fsouza/go-dockerclient v1.3.1
+	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/russross/blackfriday v2.0.0+incompatible
+	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v0.0.0-20170918181015-86672fcb3f95 // indirect
-	golang.org/x/net v0.0.0-20180208041118-f5dfe339be1d
+	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
+	golang.org/x/sys v0.0.0-20181031143558-9b800f95dbbc // indirect
 )
--- pkg/slide/collection.go
+++ pkg/slide/collection.go
@@ -1,6 +1,14 @@
 package slide
 
-import "github.com/russross/blackfriday"
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/russross/blackfriday"
+)
 
 type Collection struct {
 	Title            string
@@ -44,4 +52,55 @@ func ParseSlidesFromDom(node *blackfriday.Node, anchorNode *Slide) ([]*Slide, er
 		previousSlide = currentSlide
 	}
 	return slides, nil
+}
+
+func buildLinkChecker(workingDirectory string) func(string) (string, error) {
+	return func(href string) (string, error) {
+		hrefParsed, err := url.Parse(href)
+		if err != nil {
+			return "", fmt.Errorf("invalid url")
+		}
+		if hrefParsed.Scheme != "" {
+			return "", fmt.Errorf("has a scheme")
+		}
+		fullpath := filepath.Clean(filepath.Join(workingDirectory, hrefParsed.Path))
+		relpath, err := filepath.Rel(workingDirectory, fullpath)
+		if err != nil {
+			return "", fmt.Errorf("could not be made relative")
+		}
+		if strings.HasPrefix(relpath, "..") {
+			return "", fmt.Errorf("is in a parent directory")
+		}
+		s, err := os.Stat(fullpath)
+		if err != nil {
+			return "", fmt.Errorf("does not exist")
+		}
+		if s.IsDir() {
+			return "", fmt.Errorf("is a directory")
+		}
+		return relpath, nil
+	}
+}
+
+func (c *Collection) CollectReferencedStaticFiles() ([]string, error) {
+	seen := make(map[string]bool)
+	f := buildLinkChecker(c.WorkingDirectory)
+	for _, s := range c.Slides {
+		s.Node.Walk(func(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+			switch node.Type {
+			case blackfriday.Image:
+				href := string(node.LinkData.Destination)
+				if p, err := f(href); err == nil {
+					seen[p] = true
+				}
+			default:
+			}
+			return blackfriday.GoToNext
+		})
+	}
+	out := make([]string, 0, len(seen))
+	for k := range seen {
+		out = append(out, k)
+	}
+	return out, nil
 }
